30023708195b094f0002f28fe336e1236d82a4f1
dailcoyote <yergaliyev.production@gmail.com>
2024-08-02 01:44:55 +0500

    @new asset {hdr map} & background scene

--- main.js
+++ main.js
@@ -2,60 +2,78 @@ import './style.css'
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 
 // Камера
 const canvas = document.querySelector('canvas');
-const loader = new GLTFLoader();
 const camera = new THREE.PerspectiveCamera(
   50,
   window.innerWidth / window.innerHeight,
   0.1,
-  2000);
+  1000);
 const scene = new THREE.Scene();
-camera.position.z = 10;
+camera.position.x = Math.PI / 2;
+camera.position.z = Math.PI * 1.5;
+camera.position.y = Math.PI;
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
 let character, mixer, clock = new THREE.Clock();
 
-loader.load('model.glb', function (gltf) {
+new RGBELoader().load('skyes.hdr', texture => {
+  const gen = new THREE.PMREMGenerator(renderer)
+  const envMap = gen.fromEquirectangular(texture).texture
+  texture.dispose()
+  gen.dispose()
+  scene.environment = envMap
+  scene.background = envMap
+});
+
+new GLTFLoader().load('model.glb', function (gltf) {
   character = gltf.scene;  // character 3D object is loaded
   // character.scale.set(2, 2, 2);
-  // character.position.y = 4;
+  character.position.x = 0;
+  character.position.y = 0;
   console.log("added", character)
   console.log("animations", gltf.animations)
-  
+
   mixer = new THREE.AnimationMixer(character);
-  let runAnimationEffect = gltf.animations.find( clip => clip.name === 'run');
-  let clip = mixer.clipAction(runAnimationEffect);
-  clip.play();
-  
+  let runAnimationEffect = gltf.animations.find(clip => clip.name === 'run');
+  if (runAnimationEffect) {
+    let clip = mixer.clipAction(runAnimationEffect);
+    clip.play();
+  }
+
+  character.castShadow = true;
+  character.receiveShadow = false;
+
   scene.add(character);
 
 }, function (xhr) {
   console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
 }, function (error) {
-
   console.error(error);
-
 });
 
-// Light
-
-const light = new THREE.AmbientLight(0xd9ead3, 2);
-scene.add(light);
-
 function animate() {
-  // add to the rendering loop
-	if ( mixer ) mixer.update( clock.getDelta() );
+  if (mixer) mixer.update(clock.getDelta());
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
+
+// Window sizing
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+}, false);
 
 animate();
